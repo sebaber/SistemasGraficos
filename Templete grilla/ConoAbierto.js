4475cd53e3010372ae3cbc7312a94b449dae93fe
sebaber <vicario.sebastian@gmail.com>
2016-09-18 13:07:38 -0300

    agregado de herencia de geometrias con grilla

---
+++ Templete grilla/ConoAbierto.js
@@ -0,0 +1,33 @@
+function ConoAbierto(rows,cols,radio){
+  this.radio = radio;
+  Grilla.call(this,rows,cols);
+}
+
+inheritPrototype(ConoAbierto, Grilla);
+
+
+
+ConoAbierto.prototype._setPositionAndColorVertex = function(){
+  this.position_buffer = [];
+  this.color_buffer = [];
+
+  for (var i = 0.0; i < this.rows; i++) {
+     for (var j = 0.0; j < this.cols; j++) {
+
+         if(i!==0){
+          this.position_buffer.push(0);
+          this.position_buffer.push(0);
+          this.position_buffer.push(i);
+         }else{
+          this.position_buffer.push(this.radio*Math.sin((j*Math.PI)/ 6 ));
+          this.position_buffer.push(this.radio*Math.cos((j*Math.PI)/ 6 ));
+          this.position_buffer.push(i);
+          }
+
+         this.color_buffer.push(1.0/this.rows * i);
+         this.color_buffer.push(0.2);
+         this.color_buffer.push(1.0/this.cols * j);
+
+     }
+  }
+};
---
+++ Templete grilla/Grilla.js
@@ -0,0 +1,71 @@
+function Grilla(_rows, _cols) {
+  this.cols = _cols;
+  this.rows = _rows;
+  this.index_buffer = null;
+
+  this.position_buffer = null;
+  this.color_buffer = null;
+
+  this.webgl_position_buffer = null;
+  this.webgl_color_buffer = null;
+  this.webgl_index_buffer = null;
+
+  this._initBuffers();
+}
+
+Grilla.prototype._initBuffers = function(){
+  this._createIndexBuffer();
+  this._setPositionAndColorVertex();
+  this._setupWebGLBuffers();
+};
+
+Grilla.prototype._createIndexBuffer = function(){
+  this.index_buffer = [];
+  for(i = 0; i< (this.rows-1); ++i){
+      for(j=0;j<this.cols;++j){
+          this.index_buffer.push((i*this.cols) + j);
+          this.index_buffer.push(((i+1)*this.cols) + j);
+      }
+      this.index_buffer.push(i*this.cols);
+      this.index_buffer.push((i+1)*this.cols);
+  }
+};
+
+Grilla.prototype._setupWebGLBuffers = function(){
+    // 1. Creamos un buffer para las posicioens dentro del pipeline.
+    this.webgl_position_buffer = gl.createBuffer();
+    // 2. Le decimos a WebGL que las siguientes operaciones que vamos a ser se aplican sobre el buffer que
+    // hemos creado.
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
+    // 3. Cargamos datos de las posiciones en el buffer.
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.position_buffer), gl.STATIC_DRAW);
+
+    // Repetimos los pasos 1. 2. y 3. para la información del color
+    this.webgl_color_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_color_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.color_buffer), gl.STATIC_DRAW);
+
+    // Repetimos los pasos 1. 2. y 3. para la información de los índices
+    // Notar que esta vez se usa ELEMENT_ARRAY_BUFFER en lugar de ARRAY_BUFFER.
+    // Notar también que se usa un array de enteros en lugar de floats.
+    this.webgl_index_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.index_buffer), gl.STATIC_DRAW);
+};
+
+Grilla.prototype.draw = function(){
+  var vertexPositionAttribute = gl.getAttribLocation(glProgram, "aVertexPosition");
+  gl.enableVertexAttribArray(vertexPositionAttribute);
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
+  gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+  var vertexColorAttribute = gl.getAttribLocation(glProgram, "aVertexColor");
+  gl.enableVertexAttribArray(vertexColorAttribute);
+  gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_color_buffer);
+  gl.vertexAttribPointer(vertexColorAttribute, 3, gl.FLOAT, false, 0, 0);
+
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
+
+  // Dibujamos.
+  gl.drawElements(gl.TRIANGLE_STRIP, this.index_buffer.length-1, gl.UNSIGNED_SHORT, 0);
+};
