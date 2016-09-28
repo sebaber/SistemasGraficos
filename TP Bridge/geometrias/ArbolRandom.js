92882787d0776772ef1ed94a0a32659ae50f91f7
waraya <naraya@cys.com.ar>
2016-09-28 15:30:44 -0300

    agrego comentarios de aclaracion a la clase arbol random

--- TP Bridge/geometrias/ArbolRandom.js
+++ TP Bridge/geometrias/ArbolRandom.js
@@ -1,7 +1,8 @@
 function ArbolRandom() {
-	var randomNumber = Math.random();
+	var randomNumber = Math.random(); // 0 y 1 
 	var lista; //Lista con coordenadas r y z 
 	if (randomNumber < 0.3333){
+		//Navidad
 		lista = [0.0, 0.0 
 				,0.5, 0.0 
 				,0.5, 0.75 
@@ -10,6 +11,7 @@ function ArbolRandom() {
 				,1.4, 1.25
 				,0.0, 2.5];
 	} else if (randomNumber < 0.6666) {
+		//Puntiagudo
 		lista = [0.0, 0.0 
 				,0.35, 0.0 
 				,0.35, 0.75 
@@ -19,18 +21,25 @@ function ArbolRandom() {
 				,0.2, 2.25
 				,0.0, 2.75];
 	} else {
+		//Arbol Redondo
 		lista = [0.0, 0.0 
 				,0.5, 0.0 
 				,0.5, 1.0
-				,0.75, 1.25 
-				,1.3, 1.75 
-				,1.5, 2.25
-				,1.3, 2.75
-				,0.75, 3.25
-				,0.4, 3.4
-				,0.0, 3.5];
+				,0.75, 1.0625 
+				,1.3, 1.4875 
+				,1.5, 1.9125
+				,1.3, 2.3375
+				,0.75, 2.7625
+				,0.4, 2.89
+				,0.0, 2.975];
 	}
 	Arbol.call(this,lista);
+	var randomXZ = 0.3;//Math.random();
+	var randomY = 0.5;//Math.random();
+	var x = randomXZ;
+	var y = randomY;
+	var z = randomXZ;	
+	this.scaleNonUniform(x,y,z);
 }
 
 inheritPrototype(ArbolRandom, Arbol);
