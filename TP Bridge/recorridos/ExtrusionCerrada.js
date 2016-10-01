1e783074868524b11b031966a69f8aab4f89d4c7
sebaber <vicario.sebastian@gmail.com>
2016-10-01 12:16:42 -0300

    cilindro cerrado wachin!!!

--- TP Bridge/recorridos/ExtrusionCerrada.js
+++ TP Bridge/recorridos/ExtrusionCerrada.js
@@ -3,7 +3,7 @@ function ExtrusionCerrada(curva,forma,nlevels){
   this.nlevels = nlevels;
   this.forma = forma;
   // Modelo.call(this,this.nlevels+2,this.forma.getPoints()+1);
-  Modelo.call(this,this.nlevels,this.forma.getPoints()+1);
+  Modelo.call(this,this.nlevels+2,this.forma.getPoints()+1);
 }
 
 inheritPrototype(ExtrusionCerrada, Modelo);
@@ -15,31 +15,31 @@ ExtrusionCerrada.prototype._setPositionAndColorVertex = function(){
   this.color_buffer = [];
   vertices = this.forma.getVertices();
 
-  // pos = this.curva.getPosition(0);
-  // // matrizRotacion = Utils.getMatrizRotacion(this.curva.getTangente(0),this.curva.getNormal(0),this.curva.getBiNormal(0));
-  //
-  // var pos4 = vec4.create();
-  // vec4.set(pos4,pos[0],pos[1],pos[2],1.0);
-  //
-  // var matrizTraslacion = mat4.create();
-  // mat4.translate(matrizTraslacion,matrizTraslacion,pos4);
-  //
-  // // var matFinal = mat4.create();
-  // // mat4.multiply(matFinal,matrizTraslacion,matrizRotacion);
-  //
-  // var vertice = vec4.create();
-  // vec4.set(vertice,0.0,0.0,0.0,1.0);
-  //
-  // vec4.transformMat4(vertice,vertice,matrizTraslacion);
-  //
-  // for(var j= 0.0;j<vertices.length;j++){
-  //   this.position_buffer.push(vertice[0]);
-  //   this.position_buffer.push(vertice[1]);
-  //   this.position_buffer.push(vertice[2]);
-  //   this.color_buffer.push(1.0/this.rows * i);
-  //   this.color_buffer.push(0.2);
-  //   this.color_buffer.push(1.0/this.cols * j);
-  // }
+  pos = this.curva.getPosition(0);
+  // matrizRotacion = Utils.getMatrizRotacion(this.curva.getTangente(0),this.curva.getNormal(0),this.curva.getBiNormal(0));
+
+  var pos4 = vec4.create();
+  vec4.set(pos4,pos[0],pos[1],pos[2],1.0);
+
+  var matrizTraslacion = mat4.create();
+  mat4.translate(matrizTraslacion,matrizTraslacion,pos4);
+
+  // var matFinal = mat4.create();
+  // mat4.multiply(matFinal,matrizTraslacion,matrizRotacion);
+
+  var vertice = vec4.create();
+  vec4.set(vertice,0.0,0.0,0.0,1.0);
+
+  vec4.transformMat4(vertice,vertice,matrizTraslacion);
+
+  for(var j= 0.0;j<vertices.length;j++){
+    this.position_buffer.push(vertice[0]);
+    this.position_buffer.push(vertice[1]);
+    this.position_buffer.push(vertice[2]);
+    this.color_buffer.push(1.0/this.rows * i);
+    this.color_buffer.push(0.2);
+    this.color_buffer.push(1.0/this.cols * j);
+  }
 
   console.log("vertices: "+vertices);
   for (var i = 0.0; i < this.nlevels; i++) {
@@ -73,32 +73,33 @@ ExtrusionCerrada.prototype._setPositionAndColorVertex = function(){
       this.color_buffer.push(1.0/this.cols * j);
 
     }
-
-    // pos = this.curva.getPosition(1);
-    // matrizRotacion = Utils.getMatrizRotacion(this.curva.getTangente(1),this.curva.getNormal(1),this.curva.getBiNormal(1));
-    //
-    // var pos4 = vec4.create();
-    // vec4.set(pos4,pos[0],pos[1],pos[2],1.0);
-    //
-    // var matrizTraslacion = mat4.create();
-    // mat4.translate(matrizTraslacion,matrizTraslacion,pos4);
-    // //
-    // // var matFinal = mat4.create();
-    // // mat4.multiply(matFinal,matrizTraslacion,matrizRotacion);
-    //
-    // var vertice = vec4.create();
-    // vec4.set(vertice,0.0,0.0,0.0,1.0);
-    //
-    // vec4.transformMat4(vertice,vertice,matrizTraslacion);
-    //
-    // for(var j= 0.0;j<vertices.length;j++){
-    //   this.position_buffer.push(vertice[0]);
-    //   this.position_buffer.push(vertice[1]);
-    //   this.position_buffer.push(vertice[2]);
-    //   this.color_buffer.push(1.0/this.rows * i);
-    //   this.color_buffer.push(0.2);
-    //   this.color_buffer.push(1.0/this.cols * j);
-    // }
-
   }
+
+    pos = this.curva.getPosition(1);
+    matrizRotacion = Utils.getMatrizRotacion(this.curva.getTangente(1),this.curva.getNormal(1),this.curva.getBiNormal(1));
+
+    var pos4 = vec4.create();
+    vec4.set(pos4,pos[0],pos[1],pos[2],1.0);
+
+    var matrizTraslacion = mat4.create();
+    mat4.translate(matrizTraslacion,matrizTraslacion,pos4);
+    //
+    // var matFinal = mat4.create();
+    // mat4.multiply(matFinal,matrizTraslacion,matrizRotacion);
+
+    var vertice = vec4.create();
+    vec4.set(vertice,0.0,0.0,0.0,1.0);
+
+    vec4.transformMat4(vertice,vertice,matrizTraslacion);
+
+    for(var j= 0.0;j<vertices.length;j++){
+      this.position_buffer.push(vertice[0]);
+      this.position_buffer.push(vertice[1]);
+      this.position_buffer.push(vertice[2]);
+      this.color_buffer.push(1.0/this.rows * i);
+      this.color_buffer.push(0.2);
+      this.color_buffer.push(1.0/this.cols * j);
+    }
+
+
 };
