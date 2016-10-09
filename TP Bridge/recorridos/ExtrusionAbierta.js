5ab3289df7c26c5ba680eb0106d35044d4793732
sebaber <vicario.sebastian@gmail.com>
2016-10-08 23:25:10 -0300

    truquito piola

--- TP Bridge/recorridos/ExtrusionAbierta.js
+++ TP Bridge/recorridos/ExtrusionAbierta.js
@@ -2,10 +2,70 @@ function ExtrusionAbierta(curva,forma,nlevels){
   this.curva = curva;
   this.nlevels = nlevels;
   this.forma = forma;
-  ModeloAbierto.call(this,this.nlevels,this.forma.getPoints());
+  ModeloAbierto.call(this,this.nlevels+2,this.forma.getPoints());
 }
 
 inheritPrototype(ExtrusionAbierta, ModeloAbierto);
+
+ExtrusionAbierta.prototype.agregarPuntosIniciales = function(vertices){
+
+  var pos = [app.anchoCosta/2,0,0];
+
+  var pos4 = vec4.create();
+  vec4.set(pos4,pos[0],pos[1],pos[2],1.0);
+  console.log(pos);
+  var matrizTraslacion = mat4.create();
+  mat4.translate(matrizTraslacion,matrizTraslacion,pos4);
+
+  // var matFinal = mat4.create();
+  // mat4.multiply(matFinal,matrizTraslacion,matrizRotacion);
+
+  for (var j = 0.0; j < vertices.length; j++) {
+
+    var vertice = vec4.create();
+    vec4.set(vertice,vertices[j][0],vertices[j][1],vertices[j][2],1.0);
+    vec4.transformMat4(vertice,vertice,matrizTraslacion);
+    // console.log(vertice);
+    this.position_buffer.push(vertice[0]);
+    this.position_buffer.push(vertice[1]);
+    this.position_buffer.push(vertice[2]);
+
+    this.color_buffer.push(1.0/this.rows * i);
+    this.color_buffer.push(0.2);
+    this.color_buffer.push(1.0/this.cols * j);
+
+  }
+};
+
+ExtrusionAbierta.prototype.agregarPuntosFinales = function(vertices){
+
+  var pos = [app.anchoCosta/2,app.largoCosta,0];
+
+  var pos4 = vec4.create();
+  vec4.set(pos4,pos[0],pos[1],pos[2],1.0);
+  console.log(pos);
+  var matrizTraslacion = mat4.create();
+  mat4.translate(matrizTraslacion,matrizTraslacion,pos4);
+
+  // var matFinal = mat4.create();
+  // mat4.multiply(matFinal,matrizTraslacion,matrizRotacion);
+
+  for (var j = 0.0; j < vertices.length; j++) {
+
+    var vertice = vec4.create();
+    vec4.set(vertice,vertices[j][0],vertices[j][1],vertices[j][2],1.0);
+    vec4.transformMat4(vertice,vertice,matrizTraslacion);
+    // console.log(vertice);
+    this.position_buffer.push(vertice[0]);
+    this.position_buffer.push(vertice[1]);
+    this.position_buffer.push(vertice[2]);
+
+    this.color_buffer.push(1.0/this.rows * i);
+    this.color_buffer.push(0.2);
+    this.color_buffer.push(1.0/this.cols * j);
+
+  }
+};
 
 ExtrusionAbierta.prototype._setPositionAndColorVertex = function(){
   var pos = [];
@@ -14,6 +74,9 @@ ExtrusionAbierta.prototype._setPositionAndColorVertex = function(){
   this.color_buffer = [];
   var t;
   vertices = this.forma.getVertices();
+
+  this.agregarPuntosIniciales(vertices);
+
   for (var i = 0.0; i < this.nlevels; i++) {
     t = i/(this.nlevels-1);
     console.log("t: "+t);
@@ -46,6 +109,8 @@ ExtrusionAbierta.prototype._setPositionAndColorVertex = function(){
 
     }
   }
+
+  this.agregarPuntosFinales(vertices);
 };
 
 
