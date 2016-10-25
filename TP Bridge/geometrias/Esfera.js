da8b7cb2bcf50f9233b2f21bdf36491f13741c6a
Nicolas <nwaraya@gmail.com>
2016-10-25 00:40:33 -0300

    esferasDelTopeDeTorre

--- TP Bridge/geometrias/Esfera.js
+++ TP Bridge/geometrias/Esfera.js
@@ -5,7 +5,7 @@ function Esfera(npoints,radio, x0, y0,z) {
 	this.y0 = y0;
 	this.z = z;
 	this.vertexOfProfile = this.getVertices();
-	var color = [0.7,0.0,0.0];
+	var color = [0.2,0.1,0.1];
 	SuperficieDeRevolucion.call(this, this.vertexOfProfile,color);
 }
 
@@ -20,9 +20,11 @@ Esfera.prototype.getVertices = function() {
 	var vertices = [];
 	var d = 1.0 / this.npoints;
 	var pos;
-	for (var i = 0; i < this.npoints; i++) {
+	for (var i = 0; i <= this.npoints; i++) {
 		var t = d * i;
-    vertices.push(this.getPosition(t));
+		pos = this.getPosition(t);
+    	vertices.push(pos[0]);
+    	vertices.push(pos[1]);
 	}
 	return vertices;
 };
